85c765b4ad8bac8089dce0dfe8b0821b4f16dfbd
nunesandre13 <andre.oliv.nunes@gmail.com>
2026-04-17 00:25:33 +0100

    metrics almost done

---
+++ OmniAi-SDK/interceptors/src/jvmMain/kotlin/org/omniai/sk/gateway/telemetry/JvmTelemtryTracer.kt
@@ -0,0 +1,39 @@
+package org.omniai.sk.gateway.telemetry
+
+
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.api.trace.StatusCode
+import io.opentelemetry.extension.kotlin.asContextElement
+import kotlinx.coroutines.withContext
+import org.omniai.gateway.metrics.TelemetryTracer
+import org.omniai.sdk.core.pipeline.TelemetryTracer
+
+class JvmTelemetryTracer(
+    private val openTelemetry: OpenTelemetry,
+    instrumentationScopeName: String = "omniai-gateway-sdk"
+) : TelemetryTracer {
+
+    // Cria o tracer com o "carimbo" do seu SDK
+    private val tracer = openTelemetry.getTracer(instrumentationScopeName)
+
+    override suspend fun <T> withSpan(spanName: String, block: suspend () -> T): T {
+        // 1. Cria e inicia o Span
+        val span = tracer.spanBuilder(spanName).startSpan()
+
+        return try {
+            // 2. O TRUQUE MÁGICO: asContextElement()
+            // Isso garante que qualquer coroutine filha dentro do bloco conheça este Span
+            withContext(span.asContextElement()) {
+                block() // Executa a pipeline ou o request
+            }
+        } catch (e: Throwable) {
+            // 3. Se der erro, anota a Exception no Trace e pinta a barra de "Vermelho" no dashboard
+            span.recordException(e)
+            span.setStatus(StatusCode.ERROR, e.message ?: "Erro desconhecido")
+            throw e
+        } finally {
+            // 4. Finaliza o cronômetro do Span
+            span.end()
+        }
+    }
+}
