2c5cd43c601cb3f5fe895c8984f99fa8f3043908
Guilherme Montês Pereira Coutinho <160801637+GuilhermeA50467@users.noreply.github.com>
2026-06-28 22:54:55 +0100

    ktlint Format and ci ktlint

--- OmniAi-SDK/gateway-client/src/jsMain/kotlin/org/omniai/sdk/gateway/client/JsOmniAiGatewayRuntime.kt
+++ OmniAi-SDK/gateway-client/src/jsMain/kotlin/org/omniai/sdk/gateway/client/JsOmniAiGatewayRuntime.kt
@@ -1,33 +1,32 @@
 package org.omniai.sdk.gateway.client
 
-import org.omniai.sdk.ports.outbound.http.HttpTransportClient
-import org.omniai.sdk.gateway.client.core.OmniAiRuntime
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.promise
 import org.omniai.sdk.gateway.client.dsl.JsOmniAiConfig
+import org.omniai.sdk.ports.outbound.http.HttpTransportClient
 import kotlin.js.JsExport
 import kotlin.js.JsName
 import kotlin.js.Promise
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.promise
 
 @JsExport
 @JsName("OmniAiGatewayRuntime")
 object JsOmniAiGatewayRuntime {
-
     /**
      * Assembles the OmniAiRuntime from the JS configuration.
      * Note: This returns a Promise since JS does not have native coroutines.
-     * 
-     * In a full JS implementation, this would build the pipeline logic 
+     *
+     * In a full JS implementation, this would build the pipeline logic
      * using JS-compatible HTTP clients and services.
      */
     @JsName("assemble")
     fun assemble(
         config: JsOmniAiConfig,
-        httpClient: dynamic
-    ): Promise<dynamic> = GlobalScope.promise {
-        val transportClient = httpClient.unsafeCast<HttpTransportClient>()
-        config.config.assemble(transportClient)
-    }
+        httpClient: dynamic,
+    ): Promise<dynamic> =
+        GlobalScope.promise {
+            val transportClient = httpClient.unsafeCast<HttpTransportClient>()
+            config.config.assemble(transportClient)
+        }
 
     /**
      * Starts the OmniAi Gateway server or execution pipeline in the JS environment.
@@ -37,9 +36,10 @@ object JsOmniAiGatewayRuntime {
         config: JsOmniAiConfig,
         httpClient: dynamic,
         onStart: () -> Unit,
-        onEnd: () -> Unit = {}
-    ): Promise<Unit> = GlobalScope.promise {
-        val transportClient = httpClient.unsafeCast<HttpTransportClient>()
-        config.config.startServer(transportClient, onStart, onEnd)
-    }
+        onEnd: () -> Unit = {},
+    ): Promise<Unit> =
+        GlobalScope.promise {
+            val transportClient = httpClient.unsafeCast<HttpTransportClient>()
+            config.config.startServer(transportClient, onStart, onEnd)
+        }
 }
